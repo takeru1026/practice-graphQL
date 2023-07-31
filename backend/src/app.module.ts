e11e342581f94b7522acb268371eca9ee1a85ac8
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-08-01 08:21:47 +0900

    :tada:create model

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { join } from 'path';
       //   origin: '*',
       // },
     }),
+    TaskModule,
   ],
 })
 export class AppModule {}
