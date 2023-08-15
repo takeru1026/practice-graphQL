fc3cb2d7e6b200981e69a53a50873a3bc60a1256
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-08-16 08:09:46 +0900

    :tada:frontend create

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,15 @@ import Main from './components/Main';
 import NotFound from './components/NotFound';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import { GuestRoute, PrivateRoute } from './AuthRoute';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/signin" element={<SignIn />} />
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="/" element={<Main />} />
+				<Route path="/signin" element={<GuestRoute children={<SignIn />} />} />
+				<Route path="/signup" element={<GuestRoute children={<SignUp />} />} />
+				<Route path="/" element={<PrivateRoute children={<Main />} />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
