e95f5df3660c9b63846fc2531a1477c760cdbadb
takeru1026 <68760801+takeru1026@users.noreply.github.com>
2023-08-25 08:39:39 +0900

    :tada:taks get

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,7 +1,20 @@
+import jwtDecode from 'jwt-decode';
 import Header from './Header';
 import TaskTable from './TaskTable';
+import { Payload } from '../types/payload';
+import { useQuery } from '@apollo/client';
+import { Task } from '../types/task';
+import { GET_TASKS } from '../queries/taskQueries';
 
 const Main = () => {
+	const token = localStorage.getItem('token');
+	if (!token) return;
+	const decodedToken = jwtDecode<Payload>(token);
+	const userId = decodedToken.sub;
+
+	const { _loading, data, _error} = useQuery<{getTasks: Task[]}(GET_TASKS, variables: {userId});
+	console.log(data);
+
 	return (
 		<>
 			<Header />
---
+++ frontend/src/queries/taskQueries.ts
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const GET_TASKS = gql`
+	query getTasks($userId: Int!) {
+		getTasks(userId: $userId) {
+			Id
+			name
+			dueDate
+			description
+		}
+	}
+`;
